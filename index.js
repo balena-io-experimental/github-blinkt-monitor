db51307be6d8bf0eb93f578423475d97f4ebd9de
Tim Perry <pimterry@gmail.com>
2017-10-22 04:02:59 +0200

    Show the current github issue count, with nice animations as it changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const _ = require('_');
+const _ = require('lodash');
 const Blinkt = require('node-blinkt');
 const fetch = require('node-fetch');
 
@@ -7,9 +7,17 @@ function getIssueCount() {
     headers: new fetch.Headers({
       'authorization': 'Basic ' + new Buffer(`${process.env.GITHUB_USER}:${process.env.GITHUB_TOKEN}`).toString('base64')
     })
-  }).then((response) => response.json()).then((issues) => {
-    console.log(issues.length);
-  }).catch(console.error);
+  })
+  .then((response) => {
+    if (response.status >= 200 && response.status < 400) {
+      return response.json();
+    } else {
+      return response.json().then((error) => {
+        throw new Error(`Bad response from github: ${error.message}`);
+      });
+    }
+  })
+  .catch(console.error);
 }
 
 leds = new Blinkt();
@@ -17,15 +25,41 @@ leds.setup();
 leds.clearAll();
 leds.sendUpdate();
 
-setInterval(() => {
-  getIssueCount().then((issueCount) => {
-    console.log('Have', issueCount, 'issues');
+let existingIssueMarkers = _.range(8).map(() => null);
 
-    // TODO: Handle > 8 issues
-    _.range(issueCount).forEach((i) => {
-      leds.setPixel(i, 255, 255, 0, 1);
+setInterval(() => {
+  getIssueCount().then((issues) => {
+    let issueIds = issues.map((issue) => issue.id);
+
+    console.log('Have issues:', issueIds);
+
+    console.log('Existing issues:', existingIssueMarkers);
+
+    issueIds.filter((issueId) => !_.includes(existingIssueMarkers, issueId))
+    .forEach((issueId) => {
+      existingIssueMarkers.push(issueId);
     });
 
-    leds.sendUpdate();
+    existingIssueMarkers = existingIssueMarkers.filter((issueId) => {
+      return issueId === null || _.includes(issueIds, issueId);
+    });
+
+    console.log('Updated issues:', existingIssueMarkers);
   });
 }, 1000);
+
+setInterval(() => {
+  let firstNullIndex = existingIssueMarkers.indexOf(null);
+  if (firstNullIndex >= 0) existingIssueMarkers.splice(firstNullIndex, 1);
+  if (existingIssueMarkers.length < 8) {
+    _.range(8 - existingIssueMarkers.length)
+    .forEach(() => existingIssueMarkers.push(null));
+  }
+
+  existingIssueMarkers.slice(0, 8).forEach((issueId, i) => {
+    if (issueId !== null) leds.setPixel(i, 255, 255, 0, 0.3);
+    else leds.setPixel(i, 0, 0, 0, 1);
+  });
+
+  leds.sendUpdate();
+}, 200);
